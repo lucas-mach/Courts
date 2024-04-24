960a60a75b9dcf0c346b1874904963b5084033f5
RyanMachado <machado1.ryan@gmail.com>
2024-04-24 13:09:47 -0400

    changes to aboutus.js

--- client/src/pages/aboutus.js
+++ client/src/pages/aboutus.js
@@ -12,7 +12,12 @@ function aboutus() {
                         THE TEAM
                     </h1>
                     <p className = "homeSectionDescription">
-                        Courts was developed by a team of 4 University of Florida students.
+                        Courts was developed by a team of 4 University of Florida students:<br></br>
+                        <br></br>
+                        Lucas Mach, lead backend software engineer<br></br>
+                        Jason Hao, software engineer & scrum master<br></br>
+                        Brynn Li, lead frontend software engineer<br></br>
+                        Ryan Machado, software engineer & project manager<br></br>
                     </p>
                 </div>
             </div>
